121935a114662fbf1f49f211b73e384dfe88cd7e
leeteukki <tklohj@naver.com>
2017-07-09 19:56:29 +0900

    UiPicker

--- SelectCar_App/ViewController.swift
+++ SelectCar_App/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    let carCompanyName = ["teala", "Lamborghini", "Porsche"]
+    
+    let tesla = ["Model-S", "Model-X"]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //UIpicker datasourcs
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return carCompanyName.count
+        } else {
+            return tesla.count
+        }
+    }
+    
+    //uipicker delegate 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return carCompanyName[row]
+        }else{
+            return tesla[row]
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
